7ec227389ec08902cb95db762a37b37d021efd83
omargglez <omargglez@gmail.com>
2021-06-05 10:53:26 +0200

    feat: creted modal component

---
+++ apps/heroes-app/src/app/modules/characters/components/modal-delete/modal-delete.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { TuiDialogContext } from '@taiga-ui/core';
+import { SimpleModalComponent } from 'ngx-simple-modal';
+import { POLYMORPHEUS_CONTEXT } from '@tinkoff/ng-polymorpheus';
+
+@Component({
+  selector: 'heroes-modal-delete',
+  templateUrl: './modal-delete.component.html',
+  styleUrls: ['./modal-delete.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ModalDeleteComponent {
+  constructor(
+    @Inject(POLYMORPHEUS_CONTEXT)
+    private readonly context: TuiDialogContext<any, boolean>
+  ) {}
+
+  confirm(val: boolean) {
+    this.context.completeWith(val);
+  }
+}
